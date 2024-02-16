99acc73f2782a080d6c8f499c675dd35188e7394
Malakyasser8 <malakyasser8@gmail.com>
2024-02-16 19:20:18 +0200

    Initial commit - Finished Create Functionality

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -1,15 +1,13 @@
-const employee = [
-  { id: '1', name: 'Mohamed Sayed' },
-];
+const employee = [{ id: "1", name: "Mohamed Sayed" }];
 
 exports.getEmployees = async (req, res, next) => {
   res.status(200).json({ data: employee });
 };
 
 // TODO
-exports.deleteEmployee = async (req, res, next) => {
-};
+exports.deleteEmployee = async (req, res, next) => {};
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  employee.push(req.body);
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -1,52 +1,72 @@
 function fetchEmployees() {
-  fetch('http://localhost:3000/api/v1/employee')
-    .then(response => response.json())
-    .then(data => {
-      const tableBody = document.getElementById('dataTable')
-      tableBody.innerHTML = ''
-      const list = data.data
-      list.forEach(item => {
-        const row = document.createElement('tr')
-        const idCell = document.createElement('td')
-        idCell.textContent = item.id
-        row.appendChild(idCell)
+  fetch("http://localhost:3000/api/v1/employee")
+    .then((response) => response.json())
+    .then((data) => {
+      const tableBody = document.getElementById("dataTable");
+      tableBody.innerHTML = "";
+      const list = data.data;
+      list.forEach((item) => {
+        const row = document.createElement("tr");
+        const idCell = document.createElement("td");
+        idCell.textContent = item.id;
+        row.appendChild(idCell);
 
-        const nameCell = document.createElement('td')
-        nameCell.textContent = item.name
-        row.appendChild(nameCell)
+        const nameCell = document.createElement("td");
+        nameCell.textContent = item.name;
+        row.appendChild(nameCell);
 
-        const deleteCell = document.createElement('td')
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        const deleteCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm");
         deleteCell.appendChild(deleteButton);
 
-        row.appendChild(deleteCell)
+        row.appendChild(deleteCell);
 
-        tableBody.appendChild(row)
-      })
+        tableBody.appendChild(row);
+      });
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
 // TODO
 // add event listener to submit button
+document
+  .getElementById("SubmitButton")
+  .addEventListener("click", createEmployee);
 
 // TODO
 // add event listener to delete button
 
 // TODO
-function createEmployee (){
+function createEmployee() {
   // get data from input field
+  const name = document.getElementById("name").value;
+  const id = document.getElementById("id").value;
+  const employee = { id, name };
   // send data to BE
+  if (name && id) {
+    fetch("http://localhost:3000/api/v1/employee", {
+      method: "POST",
+      body: JSON.stringify(employee),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  } else {
+    alert("Please enter both your name and id");
+  }
   // call fetchEmployees
+  fetchEmployees();
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee() {
   // get id
   // send id to BE
   // call fetchEmployees
 }
 
-fetchEmployees()
+fetchEmployees();
